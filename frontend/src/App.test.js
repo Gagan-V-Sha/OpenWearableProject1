baa6bdfa0226bd5e4a8bf0705b1ba2889c6e4e94
Gagan <gaganvshauni@gmail.com>
2026-07-09 16:16:24 +0200

    Update project files

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders loading state on first paint', () => {
-  global.fetch = jest.fn(() => new Promise(() => {}));
-  render(<App />);
-  expect(screen.getByText(/loading your recovery data/i)).toBeInTheDocument();
-});
